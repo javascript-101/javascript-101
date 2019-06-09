ddc58dfed60645f63b32473d15332ddecc2118e0
CaroAmarillo <carolinabelenamarillo@gmail.com>
2019-06-09 16:35:57 -0300

    add let-const && change console.log to  => alert()

--- respuestas/03.js
+++ respuestas/03.js
@@ -1,24 +1,24 @@
 // Declarar una variable de nombre `cantidadDeVentanasEnMiCasa` y asignarle la cantidad de ventanas que hay en tu casa. Mostrar el valor de la variable por la terminal.
 
-var cantidadDeVentanasEnMiCasa = 3;
+let cantidadDeVentanasEnMiCasa = 3;
 
 console.log(cantidadDeVentanasEnMiCasa);
 
 // Declarar una variable de nombre `soyHumano` y asignarle el valor `true` (verdadero). Mostrar el valor de la variable por la terminal.
 
-var soyHumano = true;
+const soyHumano = true;
 
 console.log(soyHumano);
 
 // Declarar una variable de nombre `miGustoDePizzaPreferido` y asignarle la variedad de pizza que más te guste usando un `string` (cadena de caracteres). Mostrar el valor de la variable por la terminal.
 
-var miGustoDePizzaPreferido = 'Con Jamón';
+const miGustoDePizzaPreferido = 'Con Jamón';
 
 console.log(miGustoDePizzaPreferido);
 
 // Mostrar por la terminal los siguientes valores `false`, `'Hola'`, `0` y `'Minions'` usando la misma variable.
 
-var variable = false;
+let variable = false;
 console.log(variable);
 
 variable = 'Hola';
